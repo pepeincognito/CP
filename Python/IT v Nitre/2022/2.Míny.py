da729c95e9a3116bf50ee7029e292c26820bd916
oliver <oliverurdzik@gmail.com>
2022-06-08 17:03:31 +0200

    everything in IT v Nitre 2022 python done

--- Python/IT v Nitre/2022/2.Míny.py
+++ Python/IT v Nitre/2022/2.Míny.py
@@ -1,5 +1,5 @@
 n = int(input())
-lst = [str(x) for x in input().split()]
+lst = [str(x) for x in input().split(",")]
 slk = {}
 for x in range(n):
     slk[x] = [int(0) for i in range(n)]
@@ -8,9 +8,20 @@ for x in lst:
     slk[int(x[0])][int(x[1])] = 9
 
 for x in range(n):
-    for i in range(n):
-        if slk[x][i] == 9:
-            pass
+    for y in range(n):
+        if slk[x][y] == 9:
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if i + x == n or i + x == -1 or j + y == -1 or j + y == n:
+                        pass
+                    elif i == 0 and j == 0:
+                        pass
+                    else:
+                        if slk[x+i][y+j] ==9:
+                            pass
+                        else:
+                            slk[x+i][y+j] += 1
         else:
-            try:
-                if slk[x+1][i]
+            pass
+for x in slk:
+    print(" ".join([str(i) for i in slk[x]]))
